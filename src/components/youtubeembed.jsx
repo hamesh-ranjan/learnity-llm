9eddd62eb0f1c263681b10f281cfc4e88956088b
hameshranjan <94048836+hamesh-ranjan@users.noreply.github.com>
2024-08-13 13:00:22 +0530

    Update youtubeembed.jsx

--- src/components/youtubeembed.jsx
+++ src/components/youtubeembed.jsx
@@ -4,7 +4,7 @@ const YoutubeEmbed = () => (
   <div className="video-responsive">
     <iframe
       width="100%"
-      height="315"
+      height="100%"
       src={`https://www.youtube.com/embed/kqtD5dpn9C8?si=Zbz96BVHzMwnIS_P&amp;controls=0`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
