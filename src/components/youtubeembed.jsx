343c88a5bf442f531e1574ace270117f5da790b8
hameshranjan <94048836+hamesh-ranjan@users.noreply.github.com>
2024-08-13 13:05:12 +0530

    Update youtubeembed.jsx

--- src/components/youtubeembed.jsx
+++ src/components/youtubeembed.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const YoutubeEmbed = () => (
   <div className="video-responsive">
     <iframe
-      width="100%"
-      height="100%"
+      width="560px"
+      height="315px"
       src={`https://www.youtube.com/embed/kqtD5dpn9C8?si=Zbz96BVHzMwnIS_P&amp;controls=0`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
